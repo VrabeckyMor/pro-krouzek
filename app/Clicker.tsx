4887a5fc0815a9874c3f9a602a661e194cf74026
Tadeáš Kótka <tadeaskotka@gmail.com>
2025-06-06 18:40:30 +0200

    flex experimenty

--- app/Clicker.tsx
+++ app/Clicker.tsx
@@ -16,21 +16,22 @@ export default function Clicker() {
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <div className="text-2xl font-bold content-center text-center">
+            <div className="text-2xl font-bold content-center flex flex-col items-center justify-center text-center h-full w-full">
                 <h1>Clicker</h1>
-                <div>
-                    <button className="mt-10 rounded-sm p-5 bg-blue-500 hover:p-10 hover:rounded-3xl hover:mt-0 transition-all duration-500 ease-in-out" onClick={plus1}>
+                <div className="h-[150px] w-3/5 flex flex-row items-center justify-center gap-0.5">
+                    <button className="w-1/3 p-5 bg-blue-500 hover:p-10 hover:rounded-3xl hover:w-1/2 transition-all duration-500 ease-in-out active:translate-y-1 active:duration-50 active:ease-out" onClick={plus1}>
                         +1
                     </button>
-                    <button className="mt-10 rounded-sm p-5 bg-blue-500 hover:p-10 hover:rounded-3xl hover:mt-0 transition-all duration-500 ease-in-out" onClick={reset}>
+                    <button className="w-1/3 p-5 bg-blue-500 hover:p-10 hover:rounded-3xl hover:w-1/2 transition-all duration-500 ease-in-out active:translate-y-1 active:duration-50 active:ease-out" onClick={reset}>
                         reset
                     </button>
-                    <button className="mt-10 rounded-sm p-5 bg-blue-500 hover:p-10 hover:rounded-3xl hover:mt-0 transition-all duration-500 ease-in-out" onClick={minus1}>
+                    <button className="w-1/3 p-5 bg-blue-500 hover:p-10 hover:rounded-3xl hover:w-1/2 transition-all duration-500 ease-in-out active:translate-y-1 active:duration-50 active:ease-out" onClick={minus1}>
                         -1
                     </button>
                 </div>
+                <p className="text-2xl font-bold">Počet kliknutí: {count}</p>
             </div>
-            <p className="text-2xl font-bold">Počet kliknutí: {count}</p>
+            
         </div>
     );
 }
